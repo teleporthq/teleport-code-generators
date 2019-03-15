a5c89331f2d8ed4e7a2e74848b8f424ddec662b9
alexnm <alex.n.moldovan@gmail.com>
2019-03-15 12:07:04 +0200

    tests: add validator tests

---
+++ __tests__/unit/validators.ts
@@ -0,0 +1,36 @@
+import { validateComponent, validateProject } from '../../src/uidl-definitions/validators'
+// @ts-ignore
+import componentSample from '../fixtures/react-sample.json'
+// @ts-ignore
+import projectSample from '../fixtures/project-sample.json'
+
+describe('component validation', () => {
+  describe('valid', () => {
+    it('return true', () => {
+      expect(validateComponent(componentSample)).toBe(true)
+    })
+  })
+  describe('invalid', () => {
+    const badUIDL = {
+      name: 'badUIDL',
+      type: 'this should not be here',
+    }
+
+    it('return array of validation errors', () => {
+      expect(validateComponent(badUIDL)).toBeInstanceOf(Array)
+    })
+  })
+})
+
+describe('project validation', () => {
+  describe('valid', () => {
+    it('return true', () => {
+      expect(validateProject(projectSample)).toBe(true)
+    })
+  })
+  describe('invalid', () => {
+    it('return array of validation errors', () => {
+      expect(validateProject(componentSample)).toBeInstanceOf(Array)
+    })
+  })
+})
