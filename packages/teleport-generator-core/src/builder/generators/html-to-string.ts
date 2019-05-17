6c1db8d2e02ff3e44aa27f292a846aa8d1fda9f9
alexnm <alex.n.moldovan@gmail.com>
2019-05-17 17:03:15 +0300

    chore: fix tests

--- packages/teleport-generator-core/src/builder/generators/html-to-string.ts
+++ packages/teleport-generator-core/src/builder/generators/html-to-string.ts
@@ -1,5 +1,8 @@
 import toHTML from '@starptech/prettyhtml-hast-to-html'
-import { CodeGeneratorFunction, HastNode } from '../typings/generators'
+import {
+  CodeGeneratorFunction,
+  HastNode,
+} from '@teleporthq/teleport-generator-shared/lib/typings/generators'
 
 export const generator: CodeGeneratorFunction<HastNode> = (htmlObject) => {
   return toHTML(htmlObject)
