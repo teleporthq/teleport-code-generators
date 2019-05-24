dda8ac898c81150dd376ba7d42ec2ef512a14611
alexnm <alex.n.moldovan@gmail.com>
2019-05-24 17:22:00 +0300

    chore: add test project packer to replace the project-exporters

---
+++ packages/teleport-project-packer-test/src/index.ts
@@ -0,0 +1,48 @@
+import createProjectPacker from '@teleporthq/teleport-project-packer'
+
+import createReactGenerator from '@teleporthq/teleport-project-generator-react-basic'
+import createReactNextGenerator from '@teleporthq/teleport-project-generator-react-next'
+import createVueGenerator from '@teleporthq/teleport-project-generator-vue-basic'
+import createVueNuxtGenerator from '@teleporthq/teleport-project-generator-vue-nuxt'
+
+import createDiskPublisher from '@teleporthq/teleport-publisher-disk'
+import { ProjectUIDL } from '@teleporthq/teleport-generator-shared/lib/typings/uidl'
+
+import projectUIDL from '../../../examples/uidl-samples/project.json'
+
+const generators = {
+  'react-basic': createReactGenerator(),
+  'react-next': createReactNextGenerator(),
+  'vue-basic': createVueGenerator(),
+  'vue-nuxt': createVueNuxtGenerator(),
+}
+
+const packer = createProjectPacker()
+const publisher = createDiskPublisher({ outputPath: 'dist' })
+
+const packProject = async (projectType: string) => {
+  const template = {
+    templateFolder: {
+      name: projectType,
+      files: [],
+      subFolders: [],
+    },
+  }
+
+  packer.setPublisher(publisher)
+  packer.setTemplate(template)
+  packer.setGeneratorFunction(generators[projectType].generateProject)
+
+  const result = await packer.pack(projectUIDL as ProjectUIDL)
+
+  console.info(projectType, ' - ', result)
+}
+
+const run = async () => {
+  await packProject('react-basic')
+  await packProject('react-next')
+  await packProject('vue-basic')
+  await packProject('vue-nuxt')
+}
+
+run()
