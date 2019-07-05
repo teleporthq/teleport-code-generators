a4ea11e6a8bc7f548b0e85ee7898567fe91a2648
JayaKrishna <namburu1995@gmail.com>
2019-07-05 15:14:16 +0530

    chore(comp-gen-ang): Added typescript prettier

--- packages/teleport-component-generator-angular/src/index.ts
+++ packages/teleport-component-generator-angular/src/index.ts
@@ -2,10 +2,14 @@ import { createComponentGenerator } from '@teleporthq/teleport-component-generat
 
 import angularComponentPlugin from '@teleporthq/teleport-plugin-angular-base-component'
 
+import prettierTS from '@teleporthq/teleport-postprocessor-prettier-ts'
+
 export const createAngularComponentGenerator = () => {
   const generator = createComponentGenerator()
 
   generator.addPlugin(angularComponentPlugin)
+
+  generator.addPostProcessor(prettierTS)
 
   return generator
 }
