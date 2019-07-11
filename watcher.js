794bfae06fae6924a067a2250e6b2a11ebac94a2
JayaKrishna <namburu1995@gmail.com>
2019-07-11 14:49:34 +0530

    chore(watcher.js): Added a watcher to watch file changes and rebuild the package

---
+++ watcher.js
@@ -0,0 +1,25 @@
+const chokidar  = require('chokidar')
+const { exec } = require('child_process')
+const chalk = require('chalk')
+const log = console.log
+
+const watcher = chokidar.watch('packages/**/src/*.ts', { 
+  persistent: true ,
+  usePolling: true,
+  interval: 500
+})
+
+log(chalk.yellow.bold('Watching all files...'))
+
+watcher.on('change', filePath => {
+  log(chalk.redBright.bold(`File ${filePath} is changed`))
+
+  const path = filePath.split('/')
+  const location = `${path[0]}/${path[1]}/`
+
+  exec(`tsc`, { cwd: location}, (err) => {
+    if (!err ||  err === null) {
+      log(chalk.greenBright(`Package ${path[1]} is re-built`))
+    }
+  })
+})
