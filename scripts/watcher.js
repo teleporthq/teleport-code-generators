3f2a19401435afb08ac58845ead87ac66f6de5c3
JayaKrishna <namburu1995@gmail.com>
2019-07-12 11:37:04 +0530

    refactor(scrpts/watcher): Throwing error when compilation fails

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -9,17 +9,20 @@ const watcher = chokidar.watch('packages/**/src/*.ts', {
   interval: 500
 })
 
-log(chalk.yellow.bold('Watching all files...'))
+log(chalk.yellow.bold('Watching all files... 👀'))
 
 watcher.on('change', filePath => {
-  log(chalk.redBright.bold(`File ${filePath} is changed`))
+  log(chalk.redBright.bold(`${filePath} is changed`))
 
   const path = filePath.split('/')
   const location = `${path[0]}/${path[1]}/`
 
-  exec(`tsc`, { cwd: location}, (err) => {
+  exec(`tsc`, { cwd: location}, (err, stdout) => {
     if (!err ||  err === null) {
       log(chalk.greenBright(`Package ${path[1]} is re-built`))
+    } else {
+      log(chalk.white(err))
+      log(chalk.white(stdout))
     }
   })
 })
