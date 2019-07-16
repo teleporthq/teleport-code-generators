cf4c640897be79f0664b3734ca2c16a7a6a61eef
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-07-16 14:53:16 +0300

    feat(build): experimental tasks for react generator

---
+++ packages/teleport-component-generator-react/esm/index.d.ts
@@ -0,0 +1,7 @@
+import { ComponentGenerator, Mapping } from '@teleporthq/teleport-types'
+export declare const createReactComponentGenerator: (
+  variation?: string,
+  mapping?: Mapping
+) => ComponentGenerator
+declare const _default: ComponentGenerator
+export default _default
