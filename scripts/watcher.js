55da6ec956944cbe767fab7e70ae20f430cf15d7
JayaKrishna <namburu1995@gmail.com>
2019-07-18 11:53:04 +0530

    refactor(repl-component): Watcher first builds all packages before watching
    refactor(watcher): Added array to add files to ignore

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -3,26 +3,49 @@ const { exec } = require('child_process')
 const chalk = require('chalk')
 const log = console.log
 
-const watcher = chokidar.watch('packages/**/src/*.ts', { 
-  persistent: true ,
-  usePolling: true,
-  interval: 500
+const ignorePackages = [
+  'teleport-repl-component',
+  'teleport-project-packer-test'
+]
+
+log(chalk.greenBright('Building all packges, please wait for a moment...'))
+
+exec('lerna run build', (err, stdout, stderr) => {
+  if (!err || err === null) {
+    createWatcher()
+  } else {
+    displayError(err, stdout, stderr)
+  }
 })
 
-log(chalk.yellow.bold('Watching all files... 👀'))
-
-watcher.on('change', filePath => {
-  log(chalk.redBright.bold(`${filePath} is changed`))
-
-  const path = filePath.split('/')
-  const location = `${path[0]}/${path[1]}/`
-
-  exec(`tsc`, { cwd: location}, (err, stdout) => {
-    if (!err ||  err === null) {
-      log(chalk.greenBright(`Package ${path[1]} is re-built`))
-    } else {
-      log(chalk.white(err))
-      log(chalk.white(stdout))
+createWatcher = () => {
+  const watcher = chokidar.watch('packages/**/src/*.ts', { 
+    persistent: true ,
+    usePolling: true,
+    interval: 500
+  })
+  
+  log(chalk.yellow.bold('Watching all files... 👀'))
+  
+  watcher.on('change', filePath => {
+    const path = filePath.split('/')
+    const location = `${path[0]}/${path[1]}/`
+    
+    if (!ignorePackages.includes(path[1])) {
+      log(chalk.redBright.bold(`${filePath} is changed`))
+      exec(`tsc`, { cwd: location}, (err, stdout, stderr) => {
+        if (!err ||  err === null) {
+          log(chalk.greenBright(`Package ${path[1]} is re-built`))
+        } else {
+          displayError(err, stdout, stderr)
+        }
+      })
     }
   })
-})
+}
+
+displayError = (err, stdout, stderr) => {
+  log(chalk.white(err))
+  log(chalk.white(stdout))
+  log(chalk.white(stderr))
+}
