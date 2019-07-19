be86bf27f927de51cd809ec64f0f90a4a1ccc40c
alexnm <alex.n.moldovan@gmail.com>
2019-07-19 10:04:36 +0300

    chore: remove esm from version control

--- packages/teleport-component-generator-react/esm/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentGenerator, Mapping } from '@teleporthq/teleport-types'
-export declare const createReactComponentGenerator: (
-  variation?: string,
-  mapping?: Mapping
-) => ComponentGenerator
-declare const _default: ComponentGenerator
-export default _default
