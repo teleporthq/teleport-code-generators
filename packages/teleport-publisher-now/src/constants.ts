90e3ef89b8f7e4a3da9ed2d2a006781632f7356f
Utwo <mihai.legat@gmail.com>
2019-09-16 11:30:51 +0300

    Update API version and remove builder from now.json (#359)

--- packages/teleport-publisher-now/src/constants.ts
+++ packages/teleport-publisher-now/src/constants.ts
@@ -1,16 +1,7 @@
-export const DEPLOY_URL = 'https://api.zeit.co/v6/now/deployments'
-
-export const NEXT_CONFIG_FILE = {
-  file: 'next.config.js',
-  data: 'module.exports = { target: "serverless" }',
-}
+export const DEPLOY_URL = 'https://api.zeit.co/v9/now/deployments'
 
 export const NEXT_CONFIG = {
   version: 2,
-  env: {
-    NODE_ENV: 'production',
-  },
-  builds: [{ src: 'next.config.js', use: '@now/next' }, { src: 'static/*', use: '@now/static' }],
   public: true,
 }
 
