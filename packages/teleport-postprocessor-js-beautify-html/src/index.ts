256ef0b11d876a80123219c8e8c2823b5b14c8e5
JayaKrishna <namburu1995@gmail.com>
2019-09-24 11:38:40 +0530

    feat(js-beautifier): using js-beautifier for formatting html

---
+++ packages/teleport-postprocessor-js-beautify-html/src/index.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+import { html as beautifyHTML } from 'js-beautify'
+
+import { PostProcessor, PrettierFormatOptions, FileType } from '@teleporthq/teleport-types'
+
+interface PostProcessorFactoryOptions {
+  fileType?: string
+  formatOptions?: PrettierFormatOptions
+}
+
+export const createPostProcessor = (options: PostProcessorFactoryOptions = {}) => {
+  const fileType = options.fileType || FileType.HTML
+
+  const processor: PostProcessor = (codeChunks) => {
+    if (codeChunks[fileType]) {
+      const result = beautifyHTML(codeChunks[fileType], {
+        wrap_line_length: 80,
+        end_with_newline: true,
+        preserve_newlines: true,
+      })
+      codeChunks[fileType] = result
+    } else {
+      console.warn('No code chunk of type HTML found, prettier-html did not perform any operation')
+    }
+
+    return codeChunks
+  }
+
+  return processor
+}
+
+export default createPostProcessor()
