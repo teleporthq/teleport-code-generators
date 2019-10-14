3b5a0fc95ebdf7902487e0a795fad11dbdd7f3c5
JayaKrishna <namburu1995@gmail.com>
2019-10-14 21:56:17 +0530

    feat(proj-gen-gatsby): creating custom html js file for additational control

--- packages/teleport-project-generator-gatsby/src/index.ts
+++ packages/teleport-project-generator-gatsby/src/index.ts
@@ -9,12 +9,12 @@ import reactAppRoutingPlugin from '@teleporthq/teleport-plugin-react-app-routing
 import importStatementsPlugin from '@teleporthq/teleport-plugin-import-statements'
 import headConfigPlugin from '@teleporthq/teleport-plugin-jsx-head-config'
 import prettierJS from '@teleporthq/teleport-postprocessor-prettier-js'
-import prettierHTML from '@teleporthq/teleport-postprocessor-prettier-html'
 
 import { Mapping } from '@teleporthq/teleport-types'
 
 import GatsbyProjectMapping from './gatsby-mapping.json'
 import GatsbyTemplate from './project-template'
+import { createCustomHTMLEntryFile } from './utils'
 
 const createGatsbyProjectGenerator = () => {
   const reactComponentGenerator = createReactComponentGenerator(ReactStyleVariation.CSSModules)
@@ -31,7 +31,7 @@ const createGatsbyProjectGenerator = () => {
   routingComponentGenerator.addPostProcessor(prettierJS)
 
   const htmlFileGenerator = createComponentGenerator()
-  htmlFileGenerator.addPostProcessor(prettierHTML)
+  htmlFileGenerator.addPostProcessor(prettierJS)
 
   const generator = createProjectGenerator({
     components: {
@@ -47,8 +47,9 @@ const createGatsbyProjectGenerator = () => {
     },
     entry: {
       generator: htmlFileGenerator,
-      path: ['public'],
-      fileName: 'index',
+      path: ['src'],
+      fileName: 'html',
+      chunkGenerationFunction: createCustomHTMLEntryFile,
     },
     static: {
       prefix: '',
