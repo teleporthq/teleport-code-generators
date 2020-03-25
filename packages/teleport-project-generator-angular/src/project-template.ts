8c5060e43b947d516f7cb7ff59b622eb2b9db058
Jaya Krishna Namburu <namburu1995@gmail.com>
2020-03-25 22:41:11 +0530

    chore: Upgrading angular template to 9 (#440)

--- packages/teleport-project-generator-angular/src/project-template.ts
+++ packages/teleport-project-generator-angular/src/project-template.ts
@@ -18,28 +18,28 @@ export default {
   },
   "private": true,
   "dependencies": {
-    "@angular/common": "~8.2.3",
-    "@angular/compiler": "~8.2.3",
-    "@angular/core": "~8.2.3",
-    "@angular/forms": "~8.2.3",
-    "@angular/platform-browser": "~8.2.3",
-    "@angular/platform-browser-dynamic": "~8.2.3",
-    "@angular/router": "~8.2.3",
-    "rxjs": "~6.4.0",
+    "@angular/common": "~9.0.7",
+    "@angular/compiler": "~9.0.7",
+    "@angular/core": "~9.0.7",
+    "@angular/forms": "~9.0.7",
+    "@angular/platform-browser": "~9.0.7",
+    "@angular/platform-browser-dynamic": "~9.0.7",
+    "@angular/router": "~9.0.7",
+    "rxjs": "~6.5.4",
     "tslib": "^1.10.0",
-    "zone.js": "~0.9.1",
-    "core-js": "^3.0.1"
+    "core-js": "~3.0.1",
+    "zone.js": "~0.10.2"
   },
   "devDependencies": {
-    "@angular-devkit/build-angular": "~0.803.0",
-    "@angular/cli": "~8.3.0",
-    "@angular/compiler-cli": "~8.2.3",
-    "@angular/language-service": "~8.2.3",
-    "@types/node": "~8.9.4",
-    "codelyzer": "^5.0.0",
-    "ts-node": "~7.0.0",
-    "tslint": "~5.15.0",
-    "typescript": "^3.7.5"
+    "@angular-devkit/build-angular": "~0.900.7",
+    "@angular/cli": "~9.0.7",
+    "@angular/compiler-cli": "~9.0.7",
+    "@angular/language-service": "~9.0.7",
+    "@types/node": "^12.11.1",
+    "codelyzer": "^5.1.2",
+    "ts-node": "~8.3.0",
+    "tslint": "~5.18.0",
+    "typescript": "~3.7.5"
   }
 }`,
       fileType: 'json',
@@ -401,7 +401,7 @@ Thumbs.db`,
         {
           name: 'main',
           content: `
-import './polyfills'
+import './polyfills';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
