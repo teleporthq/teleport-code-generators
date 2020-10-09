a88e4306164aed8a08f9ce97f49344cb3345dee9
teeteejo <72230915+teeteejo@users.noreply.github.com>
2020-10-09 08:11:14 +0100

    Fix typo (#485)
    * Fix typo
    
    Fixes #469
    
    * Fix typo

--- packages/teleport-plugin-common/src/index.ts
+++ packages/teleport-plugin-common/src/index.ts
@@ -1,13 +1,13 @@
 import * as ASTBuilders from './builders/ast-builders'
 import * as StyleBuilders from './builders/style-builders'
-import * as HASTBuilers from './builders/hast-builders'
+import * as HASTBuilders from './builders/hast-builders'
 
 import * as ASTUtils from './utils/ast-utils'
 import * as StyleUtils from './utils/style-utils'
 import * as HASTUtils from './utils/hast-utils'
 import ParsedASTNode from './utils/parsed-ast'
 
-export { ASTBuilders, StyleBuilders, HASTBuilers, ASTUtils, StyleUtils, HASTUtils, ParsedASTNode }
+export { ASTBuilders, StyleBuilders, HASTBuilders, ASTUtils, StyleUtils, HASTUtils, ParsedASTNode }
 
 export { default as createHTMLTemplateSyntax } from './node-handlers/node-to-html'
 export { default as createJSXSyntax } from './node-handlers/node-to-jsx'
