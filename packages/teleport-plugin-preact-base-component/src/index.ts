3f54433dd023dc2c5377c4e371084cbdcd8812c3
Jaya Krishna Namburu <namburu1995@gmail.com>
2022-02-24 17:28:30 +0530

    refactor(plugin-jsx): add props attr by default to any component (#651)

--- packages/teleport-plugin-preact-base-component/src/index.ts
+++ packages/teleport-plugin-preact-base-component/src/index.ts
@@ -73,7 +73,6 @@ export const createPreactComponentPlugin: ComponentPluginFactory<PreactPluginCon
     const preactComponent = ASTUtils.createPureComponent(
       componentName,
       stateDefinitions,
-      propDefinitions,
       jsxTagStructure
     )
 
