11dc230ac77033f8426a2508efc9a5b9d0e79861
JayaKrishnaNamburu <namburu1995@gmail.com>
2022-03-09 12:33:01 +0530

    feat(chore): testing out turorepo for decreasing build times

--- scripts/watcher.mjs
+++ scripts/watcher.mjs
@@ -4,7 +4,6 @@ import { exec } from 'child_process'
 
 const log = console.log
 
-const ignorePackages = ['teleport-repl-component', 'teleport-test']
 const watcher = chokidar.watch(['packages/**/src/**/*.ts', 'packages/**/src/**/*.json'], {
   depth: 4,
   persistent: true,
@@ -15,19 +14,9 @@ const watcher = chokidar.watch(['packages/**/src/**/*.ts', 'packages/**/src/**/*
 log(chalk.yellow.bold('Watching all files... 👀'))
 
 watcher.on('change', async (filePath) => {
-  const splitPath = filePath.split('/')
-  const location = `${splitPath[0]}/${splitPath[1]}/`
-  const fileName = splitPath[1]
-
-  if (ignorePackages.includes(fileName)) {
-    return
-  }
-
-  log(chalk.yellow(`Changes detected in ${fileName}`))
-
-  exec(`yarn build`, { cwd: location }, (err, stdout, stderr) => {
+  exec(`yarn build`, {}, (err, stdout, stderr) => {
     if (!err || err === null) {
-      log(chalk.greenBright(`${splitPath[1]}'s types was successfully re-built`))
+      log(stdout)
     } else {
       log(err, stdout, stderr)
     }
