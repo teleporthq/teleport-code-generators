b1f7576e9bc06df1ecfc1fca1d156be0d2a82530
Jaya Krishna Namburu <namburu1995@gmail.com>
2022-05-04 16:32:19 +0530

    feat(nextjs-tailwind): a project plugin for using tailwind with nextjs projects (#675)
    * feat(nextjs-tailwind): a project plugin for using tailwind with nextjs projects
    
    * Fixing tests on tailwind-nextjs project plugin

---
+++ packages/teleport-project-plugin-next-tailwind/__tests__/index.ts
@@ -0,0 +1,65 @@
+import {
+  FileType,
+  InMemoryFileRecord,
+  ProjectPluginStructure,
+  ProjectStrategy,
+  ProjectUIDL,
+} from '@teleporthq/teleport-types'
+import { TailwindNextJSPlugin } from '../src'
+import projectUIDL from '../../../examples/test-samples/project-sample.json'
+import { NextTemplate } from '@teleporthq/teleport-project-generator-next'
+
+describe('Plugins adds tailwind as devDependnecy when used with Next', () => {
+  const structure: ProjectPluginStructure = {
+    files: new Map<string, InMemoryFileRecord>(),
+    uidl: projectUIDL as unknown as ProjectUIDL,
+    dependencies: {},
+    devDependencies: {},
+    template: NextTemplate,
+    strategy: {} as ProjectStrategy,
+    rootFolder: { name: 'root', files: [], subFolders: [] },
+  }
+
+  it('Adds deps and changes config, when project has missing project-style sheet', async () => {
+    structure.files.set('_app', {
+      files: [
+        {
+          name: '_app',
+          fileType: FileType.JS,
+          content: ``,
+        },
+      ],
+      path: [''],
+    })
+    const plugin = new TailwindNextJSPlugin({ config: {}, css: `.tailwind-class{ }` })
+    const { files } = await plugin.runAfter(structure)
+    const appFile = files.get('_app').files[0]
+
+    expect(appFile).toBeDefined()
+    expect(appFile.content).toContain(`import "./global.css"`)
+    expect(files.size).toBe(3)
+    expect(files.get('tailwindGlobal')).toBeDefined()
+    expect(files.get('tailwindConfig')).toBeDefined()
+  })
+
+  it('Adds deps and changes config, when the project styles are present', async () => {
+    structure.files.delete('_app')
+    structure.files.set('projectStyleSheet', {
+      files: [
+        {
+          name: 'style',
+          fileType: FileType.CSS,
+          content: ``,
+        },
+      ],
+      path: [''],
+    })
+
+    const plugin = new TailwindNextJSPlugin({ config: {}, css: `.tailwind-class{ }` })
+    const { files } = await plugin.runAfter(structure)
+
+    const styleFile = files.get('projectStyleSheet')
+    expect(styleFile).toBeDefined()
+    expect(files.size).toBe(3)
+  })
+})
