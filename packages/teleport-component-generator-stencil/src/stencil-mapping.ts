722c659835f5e291b0e0ff74ec14c43af07ffbbc
Eliza N <eliza.nitoi@gmail.com>
2022-11-07 14:44:19 +0200

    Update dang-html library

--- packages/teleport-component-generator-stencil/src/stencil-mapping.ts
+++ packages/teleport-component-generator-stencil/src/stencil-mapping.ts
@@ -7,7 +7,7 @@ export const StencilMapping: Mapping = {
       dependency: {
         type: 'package',
         path: 'dangerous-html',
-        version: '0.1.10',
+        version: '0.1.11',
         meta: {
           importJustPath: true,
         },
