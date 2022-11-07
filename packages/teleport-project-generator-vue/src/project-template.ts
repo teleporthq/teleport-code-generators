618d96cb04d4170a9cf4002546536ef363bcc9d6
Eliza Nitoi <eliza.nitoi@gmail.com>
2022-11-07 16:25:52 +0200

    Fix build when project name contains paranthesis (#722)

--- packages/teleport-project-generator-vue/src/project-template.ts
+++ packages/teleport-project-generator-vue/src/project-template.ts
@@ -39,7 +39,9 @@ export default {
     {
       name: 'vue.config',
       fileType: 'js',
-      content: `module.exports = {
+      content: String.raw`const path = require('path')
+
+module.exports = {
   css: {
     loaderOptions: {
       css: {
@@ -47,6 +49,15 @@ export default {
       },
     },
   },
+  chainWebpack: config => {
+    config.plugin('copy').tap(args => {
+      const UNESCAPED_GLOB_SYMBOLS_RE = /(\\?)([()*?[\]{|}]|^!|[!+@](?=\())/g;
+      const publicDir = path.resolve(process.VUE_CLI_SERVICE.context, 'public').replace(/\\/g, '/');
+      const escapePublicDir= publicDir.replace(UNESCAPED_GLOB_SYMBOLS_RE, '\\$2');
+      args[0].patterns[0].globOptions.ignore = args[0].patterns[0].globOptions.ignore.map(i => i.replace(publicDir, escapePublicDir));
+      return args;
+  });
+  }
 };`,
     },
     {
