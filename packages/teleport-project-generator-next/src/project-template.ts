f041e8b70b09c034b05ea4679d4bb9c5efd0041f
Jaya Krishna <namburu1995@gmail.com>
2023-04-19 16:55:33 +0200

    Initial setup for next fonts package

--- packages/teleport-project-generator-next/src/project-template.ts
+++ packages/teleport-project-generator-next/src/project-template.ts
@@ -18,16 +18,14 @@ export default {
   "author": "TeleportHQ",
   "license": "MIT",
   "dependencies": {
-    "next": "^12.1.0",
-    "react": "^17.0.2",
-    "react-dom": "^17.0.2",
+    "next": "^13.3.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
     "@lottiefiles/lottie-player": "1.6.0"
   }
 }`,
       fileType: 'json',
     },
-<<<<<<< Updated upstream
-=======
     {
       name: 'next.config',
       fileType: 'js',
@@ -35,7 +33,6 @@ export default {
   reactStrictMode: true,
 };`,
     },
->>>>>>> Stashed changes
   ],
   subFolders: [],
 }
