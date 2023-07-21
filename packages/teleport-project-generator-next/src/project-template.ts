a4d74bfe222ff476697bdac193ff56dd2c2232c0
Jaya Krishna <namburu1995@gmail.com>
2023-07-21 19:11:25 +0530

    (fix): switch back to next@12

--- packages/teleport-project-generator-next/src/project-template.ts
+++ packages/teleport-project-generator-next/src/project-template.ts
@@ -20,9 +20,9 @@ const projectTemplate: GeneratedFolder = {
   "author": "TeleportHQ",
   "license": "MIT",
   "dependencies": {
-    "next": "^13.4.10",
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0"
+    "next": "^12.1.10",
+    "react": "^17.0.2",
+    "react-dom": "^17.0.2"
   }
 }`,
       fileType: FileType.JSON,
--- packages/teleport-project-plugin-next-cache/src/index.ts
+++ packages/teleport-project-plugin-next-cache/src/index.ts
@@ -20,21 +20,21 @@ export class ProjectPluginNextCache implements ProjectPlugin {
         {
           name: 'revalidate',
           fileType: FileType.JS,
-          content: `import { revalidate } from "${dependency.path}"
-import { revalidatePath } from 'next/cache'
+          content: `import { ${dependency?.meta?.originalName ?? 'revalidate'} } from "${
+            dependency.path
+          }"
 import routeMappers from '../../teleport-config.json'
 
 export default async function handler(req, res) {
   try {
     const pathsToRevalidate = await revalidate(req, routeMappers);
-
     if (pathsToRevalidate.length == 0) {
       return res.status(400).json({ revalidated: false, message: "No paths to revalidate" });
     }
 
     pathsToRevalidate.forEach((path) => {
       console.log("[ON-DEMAND_ISR]: Clearing cahce for path", path)
-      revalidatePath(path)
+      req.revalidate(path)
     })
     return res.status(200).json({ revalidated: true });
   } catch {
