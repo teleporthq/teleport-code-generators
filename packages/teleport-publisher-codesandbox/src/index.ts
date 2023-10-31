7eca66a612702a7bda13718855990a0d57c31498
Jaya Krishna <namburu1995@gmail.com>
2023-10-31 21:49:14 +0530

    (chore): Revert to client side sandboxes

--- packages/teleport-publisher-codesandbox/src/index.ts
+++ packages/teleport-publisher-codesandbox/src/index.ts
@@ -38,7 +38,7 @@ export const createCodesandboxPublisher: PublisherFactory<
         'Content-Type': 'application/json',
       },
       method: 'POST',
-      body: JSON.stringify({ files: flatProject, environment: 'server' }),
+      body: JSON.stringify({ files: flatProject }),
     })
 
     if (response.status >= 500) {
