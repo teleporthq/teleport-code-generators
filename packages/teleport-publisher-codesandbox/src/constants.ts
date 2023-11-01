0997a8e2c55a75d82a9f3ae076e7c1970d846e17
Jaya Krishna Namburu <namburu1995@gmail.com>
2023-11-01 11:52:28 +0530

    (chore): Revert to client side sandboxes (#876)

--- packages/teleport-publisher-codesandbox/src/constants.ts
+++ packages/teleport-publisher-codesandbox/src/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://codesandbox.io/api/v1/sandboxes/define'
 
-export const BASE_SANDBOX_URL = 'https://codesandbox.io/p/sandbox/'
+export const BASE_SANDBOX_URL = 'https://codesandbox.io/s/'
