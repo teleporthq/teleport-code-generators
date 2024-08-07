8ed0ae437ed040e6484bd015597d65785780cf14
Jaya Krishna <namburu1995@gmail.com>
2024-08-07 15:10:25 +0530

    update watcher for build projects

--- scripts/watcher.mjs
+++ scripts/watcher.mjs
@@ -30,18 +30,14 @@ watcher.on('change', async (filePath) => {
       log(chalk.greenBright(`${splitPath[1]}'s types was successfully re-built`))
       log(chalk.blueBright(`Generating code....`))
 
-      exec(
-        `yarn standalone && yarn cms`,
-        { cwd: 'packages/teleport-test' },
-        (err, stdout, stderr) => {
-          if (err) {
-            console.error(err)
-          }
-          console.log(stderr)
-
-          console.log(stdout)
+      exec(`yarn standalone`, { cwd: 'packages/teleport-test' }, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
         }
-      )
+        console.log(stderr)
+
+        console.log(stdout)
+      })
     } else {
       log(err, stdout, stderr)
     }
